61f517d96c96d4a8aca88c83b506c6936804112d
casablanque-code <casablanque@proton.me>
2026-05-13 23:36:50 +0300

    add runtime provider variables to docker host

--- templates/docker-host/tofu/main.tf
+++ templates/docker-host/tofu/main.tf
@@ -2,10 +2,26 @@ terraform {
   required_version = ">= 1.6.0"
 }
 
+variable "region" {
+  type = string
+}
+
+variable "shape" {
+  type = string
+}
+
 output "public_ip" {
   value = "203.0.113.10"
 }
 
 output "private_ip" {
   value = "10.0.0.10"
+}
+
+output "region" {
+  value = var.region
+}
+
+output "shape" {
+  value = var.shape
 }
